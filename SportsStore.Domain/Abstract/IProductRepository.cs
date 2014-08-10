65d61ed63c69f010a174822f32864fca606a7ebd
Sean Dooley <sean.dooley.22@gmail.com>
2014-08-10 15:28:54 +0100

    Initial commit

---
+++ SportsStore.Domain/Abstract/IProductRepository.cs
@@ -0,0 +1,13 @@
+﻿using SportsStore.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SportsStore.Domain.Abstract
+{
+    public interface IProductRepository
+    {
+        IQueryable<Product> Products { get; }
+    }
+}
---
+++ SportsStore.WebUI/Infrastructure/NinjectControllerFactory.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Ninject;
+using System.Web.Mvc;
+using Moq;
+using SportsStore.Domain.Abstract;
+using SportsStore.Domain.Entities;
+
+namespace SportsStore.WebUI.Infrastructure
+{
+    public class NinjectControllerFactory : DefaultControllerFactory
+    {
+        private IKernel ninjectKernel;
+
+        public NinjectControllerFactory()
+        {
+            ninjectKernel = new StandardKernel();
+            AddBindings();
+        }
+
+        protected override IController GetControllerInstance(System.Web.Routing.RequestContext requestContext, Type controllerType)
+        {
+            //return base.GetControllerInstance(requestContext, controllerType);
+            return controllerType == null
+                ? null
+                : (IController)ninjectKernel.Get(controllerType);
+        }
+
+        private void AddBindings()
+        {
+            //throw new NotImplementedException();
+
+            // Mock implementation of the IProductRepository Interface
+            Mock<IProductRepository> mock = new Mock<IProductRepository>();
+            mock.Setup(m => m.Products).Returns(new List<Product>
+            {
+                new Product { Name = "Football", Price = 25 },
+                new Product { Name = "Surf Board", Price = 179 },
+                new Product { Name = "Running Shoes", Price = 95 }
+            }.AsQueryable());
+            ninjectKernel.Bind<IProductRepository>().ToConstant(mock.Object);
+        }
+    }
+}
