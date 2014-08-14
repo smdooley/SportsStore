af9c63d8a3203e24572f2031ad15deb909dadea6
Sean Dooley <sean.dooley.22@gmail.com>
2014-08-14 15:25:06 +0100

    CH7 - Adding the View Model data

--- SportsStore.UnitTests/TestProductController.cs
+++ SportsStore.UnitTests/TestProductController.cs
@@ -34,10 +34,10 @@ namespace SportsStore.UnitTests
             controller.PageSize = 3;
 
             //// Action
-            IEnumerable<Product> result = (IEnumerable<Product>)controller.List(2).Model;
+            ProductsListViewModel result = (ProductsListViewModel)controller.List(2).Model;
 
             //// Assert
-            Product[] products = result.ToArray();
+            Product[] products = result.Products.ToArray();
             Assert.IsTrue(products.Length == 2);
             Assert.AreEqual(products[0].Name, "P4");
             Assert.AreEqual(products[1].Name, "P5");
@@ -65,6 +65,35 @@ namespace SportsStore.UnitTests
 
             // Assert
             Assert.AreEqual(result.ToString(), @"<a href=""Page1"">1</a><a class=""selected"" href=""Page2"">2</a><a href=""Page3"">3</a>");
+        }
+
+        [TestMethod]
+        public void Can_Send_Pagination_View_Model()
+        {
+            // Arrange - create mock repository
+            Mock<IProductRepository> mock = new Mock<IProductRepository>();
+            mock.Setup(m => m.Products).Returns(new Product[] {
+                new Product { ProductID = 1, Name = "P1" },
+                new Product { ProductID = 2, Name = "P2" },
+                new Product { ProductID = 3, Name = "P3" },
+                new Product { ProductID = 4, Name = "P4" },
+                new Product { ProductID = 5, Name = "P5" }
+            }.AsQueryable());
+
+            // Arrange - create a controller and make the page size 3 items
+            ProductController controller = new ProductController(mock.Object);
+            controller.PageSize = 3;
+
+            // Action
+            ProductsListViewModel result = (ProductsListViewModel)controller.List(2).Model;
+
+            // Assert
+            PagingInfo pageInfo = result.PagingInfo;
+
+            Assert.AreEqual(pageInfo.CurrentPage, 2);
+            Assert.AreEqual(pageInfo.ItemsPerPage, 3);
+            Assert.AreEqual(pageInfo.TotalItems, 5);
+            Assert.AreEqual(pageInfo.TotalPages, 2);
         }
     }
 }
--- SportsStore.WebUI/Global.asax.cs
+++ SportsStore.WebUI/Global.asax.cs
@@ -25,6 +25,12 @@ namespace SportsStore.WebUI
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute(
+                null,
+                "Page{page}",
+                new { Controller = "Product", action = "List" }
+            );
+
+            routes.MapRoute(
                 "Default", // Route name
                 "{controller}/{action}/{id}", // URL with parameters
                 //new { controller = "Home", action = "Index", id = UrlParameter.Optional } // Parameter defaults
