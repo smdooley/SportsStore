aa17e23e4d7cfda72e313d5e1f33516c0e35f377
Sean Dooley <sean.dooley.22@gmail.com>
2014-08-14 23:17:15 +0100

    CH7 - Added category navigation

---
+++ SportsStore.UnitTests/TestNavigationController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using SportsStore.Domain.Abstract;
+using SportsStore.Domain.Entities;
+using SportsStore.WebUI.Controllers;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SportsStore.UnitTests
+{
+    [TestClass]
+    public class TestNavigationController
+    {
+        [TestMethod]
+        public void Can_Create_Categories()
+        {
+            // Arrange - create the mock repository
+            Mock<IProductRepository> mock = new Mock<IProductRepository>();
+            mock.Setup(m => m.Products).Returns(new Product[] {
+                new Product { ProductID = 1, Name = "P1", Category = "Apples" },
+                new Product { ProductID = 2, Name = "P2", Category = "Apples" },
+                new Product { ProductID = 3, Name = "P3", Category = "Plums" },
+                new Product { ProductID = 4, Name = "P4", Category = "Oranges" },
+            }.AsQueryable());
+
+            // Arrange - create the controller
+            NavigationController controller = new NavigationController(mock.Object);
+
+            // Act - get the set of categories
+            string[] results = ((IEnumerable<string>)controller.Menu().Model).ToArray();
+
+            // Assert
+            Assert.AreEqual(results.Length, 3);
+            Assert.AreEqual(results[0], "Apples");
+            Assert.AreEqual(results[1], "Oranges");
+            Assert.AreEqual(results[2], "Plums");
+        }
+    }
+}
