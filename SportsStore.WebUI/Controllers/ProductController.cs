fae662f5472b816800275b263bc8a8276dfc9da4
Sean Dooley <sean.dooley.22@gmail.com>
2014-08-14 22:47:58 +0100

    CH7 - Added Category to pagination

--- SportsStore.WebUI/Controllers/ProductController.cs
+++ SportsStore.WebUI/Controllers/ProductController.cs
@@ -18,11 +18,12 @@ namespace SportsStore.WebUI.Controllers
             repository = productRepository;
         }
 
-        public ViewResult List(int page = 1)
+        public ViewResult List(string category, int page = 1)
         {
             ProductsListViewModel model = new ProductsListViewModel
             {
                 Products = repository.Products
+                    .Where(p => category == null || p.Category == category)
                     .OrderBy(p => p.ProductID)
                     .Skip((page - 1) * PageSize)
                     .Take(PageSize),
@@ -31,7 +32,8 @@ namespace SportsStore.WebUI.Controllers
                     CurrentPage = page,
                     ItemsPerPage = PageSize,
                     TotalItems = repository.Products.Count()
-                }
+                },
+                CurrentCategory = category
             };
             return View(model);
         }
--- SportsStore.WebUI/Global.asax.cs
+++ SportsStore.WebUI/Global.asax.cs
@@ -26,16 +26,30 @@ namespace SportsStore.WebUI
 
             routes.MapRoute(
                 null,
-                "Page{page}",
-                new { Controller = "Product", action = "List" }
+                "", // Only matches the empty URL (i.e. /)
+                new { controller = "Product", action = "List", category = (string)null, page = 1 });
+
+            routes.MapRoute(
+                null,
+                "Page{page}", // Matches /Page2, /Page123 but not /PageXYZ
+                new { controller = "Product", action = "List", category = (string)null },
+                new { page = @"\d+" } // Constraints: page must be numerical
             );
 
             routes.MapRoute(
-                "Default", // Route name
-                "{controller}/{action}/{id}", // URL with parameters
-                //new { controller = "Home", action = "Index", id = UrlParameter.Optional } // Parameter defaults
-                new { controller = "Product", action = "List", id = UrlParameter.Optional } // Parameter defaults
+                null,
+                "{category}", // Matches /Football or /AnythingWithNoSlash
+                new { controller = "Product", action = "List", page = 1 }
             );
+
+            routes.MapRoute(
+                null,
+                "{category}/Page{page}", // Matches /Football/Page567
+                new { controller = "Product", action = "List" }, // Defaults
+                new { page = @"\d+" } // Constraints: page must be numerical
+            );
+
+            routes.MapRoute(null, "{controller}/{action}");
 
         }
 
