1b1c8c50ecc510618bc9d605efdf1dc4f520605c
Sean Dooley <sean.dooley.22@gmail.com>
2014-09-03 22:29:11 +0100

    CH8 - implementing the order processor

---
+++ SportsStore.Domain/Abstract/IOrderProcessor.cs
@@ -0,0 +1,13 @@
+﻿using SportsStore.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SportsStore.Domain.Abstract
+{
+    public interface IOrderProcessor
+    {
+        void ProcessOrder(Cart cart, ShippingDetails shippingDetails);
+    }
+}
--- SportsStore.WebUI/Controllers/CartController.cs
+++ SportsStore.WebUI/Controllers/CartController.cs
@@ -72,5 +72,10 @@ namespace SportsStore.WebUI.Controllers
         {
             return PartialView(cart);
         }
+
+        public ViewResult Checkout()
+        {
+            return View(new ShippingDetails());
+        }
     }
 }
